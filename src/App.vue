a73bd64e31e8cc50ad6a51e13d6d5981e602ab21
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-09-01 15:05:38 +0200

    added usage factors and profit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <div id="bglayer1"/>
+  <div id="bglayer2"/>
   <div class="background-layer"/>
   <router-view/>
 </template>
@@ -6,27 +8,51 @@
 <style scoped lang="less">
 @import '@/colors.less';
 
-background-layer {
-	background: linear-gradient(#3279b2, #e92181, #3279b2); 
-	background-size: 200% 200%;
-	animation: gradient 35s ease infinite;
-	opacity: 0.5;
-	width: 100vh; 
-  height: 100vw; 
+#bglayer1 {
+  background: linear-gradient(#3279b2, shade(#e92181, 20%), #3279b2); 
+  background-size: 400% 400%;
+  animation: bggrad1 35s ease infinite;
+  opacity: 0.5;
+  width: 100vw; 
+  height: 100vh; 
   position: fixed; 
   left:0; 
   top:0; 
 }
 
-@keyframes gradient {
-	0% {
-		background-position: 50% 0%;
-	}
-	50% {
-		background-position: 50% 100%;
-	}
-	100% {
-		background-position: 50% 0%;
-	}
+#bglayer2 {
+  background: linear-gradient(-45deg, @c0, @c1); 
+  background-size: 400% 400%;
+  animation: bggrad2 30s ease infinite;
+  opacity: 0.5;
+  width: 100vw; 
+  height: 100vh; 
+  position: fixed; 
+  left:0; 
+  top:0; 
+}
+
+@keyframes bggrad2 {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes bggrad1 {
+  0% {
+    background-position: 50% 0%;
+  }
+  50% {
+    background-position: 50% 100%;
+  }
+  100% {
+    background-position: 50% 0%;
+  }
 }
 </style>
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-  
   import { ref } from 'vue'
+
+  class UsageFactor {
+    name = ""
+    f = 1
+  }
   
   let calcTypes = {
     recRent: {
@@ -11,14 +15,22 @@
   let calcTypeList = Object.keys(calcTypes).map(key => ({key, label: calcTypes[key].label}))
   
   let calcType = ref("recRent")
-  let rentIntervals = ['yearly', 'monthly', 'weekly']
-  let rentInterval = ref(rentIntervals[1]) 
-  let rentAmount = ref(210)
-  let rentCurrency = ref('NEAR')
   
-  let yearlyFluct = ref((new Array(12)).fill(100))
-  let weeklyFluct = ref((new Array(7)).fill(100))
-  let dailyFluct = ref((new Array(24)).fill(100))
+
+  let rentIntervals = ['yearly', 'monthly', 'weekly']
+  let recRent = ref({
+    rentInterval: rentIntervals[1], 
+    rentAmount: 210,
+    rentCurrency: 'NEAR',
+    
+    yearlyFluct: (new Array(12)).fill(100),
+    weeklyFluct: (new Array(7)).fill(100),
+    dailyFluct: (new Array(24)).fill(100),
+    
+    profit: 0, 
+
+    usageFactors: [] as UsageFactor[]
+  })
   
 </script>
 
@@ -39,121 +51,134 @@
     v-model="calcType"
     :items="calcTypeList"></v-select>
     <template v-if="calcType == 'recRent'" class="calcDetails">
-      <p>How do you pay the rent for this resource?</p>
+      <h3>How do you pay the rent for this resource?</h3>
       <v-text-field
       label="rent interval"
       variant="outlined"
-      v-model="rentInterval"
+      v-model="recRent.rentInterval"
       ></v-text-field>
       <v-text-field
       label="rent amount"
       variant="outlined"
-      v-model="rentAmount"
+      v-model="recRent.rentAmount"
       > </v-text-field>
       <v-text-field
       label="rent currency"
       variant="outlined"
-      v-model="rentCurrency"
+      v-model="recRent.rentCurrency"
       > </v-text-field>
-      <p>Estimate the resource demand in percent</p>
-      
-      <v-expansion-panels dense>
-        <v-expansion-panel>
-          <v-expansion-panel-title>
-            Yearly fluctuation 
-          </v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-container fluid>
-              <v-row no-gutters>
-                <v-col 
-                  cols="4"
-                  :key="i"
-                  v-for="(item,i) in 12"
-                >
-                  <v-text-field
-                  variant="outlined"          
-                  suffix="%"
-                  v-model="yearlyFluct[i]"
-                  :label="(new Date(1993, i, 13)).toLocaleString('default', { month: 'long' })"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
-      <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-title>
-            Weekly fluctuation
-          </v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-container fluid>
-              <v-row no-gutters>
-                <v-col 
-                cols="4"
-                :key="i"
-                v-for="(item,i) in 7"
-                >
-                  <v-text-field
-                  suffix="%"
-                  variant="outlined"
-                  v-model="weeklyFluct[i]"
-                  :label="(new Date(2022, 8 - 1, 29 - 7 + i)).toLocaleString('default', { weekday: 'long' })"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
-      <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-title>
-            Daily fluctuation
-          </v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-container fluid>
-              <v-row no-gutters>
-                <v-col 
-                cols="4"
-                :key="i"
-                v-for="(item,i) in 24"
-                >
-                  <v-text-field
-                  suffix="%"
-                  variant="outlined"
-                  v-model="dailyFluct[i]"
-                  :label="i + ':00'"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
+      <h3>How much profit would you like to make?</h3>
+      <p>0.7% is forwarded to the developers and maintainers of this dApp.</p>
+      <v-text-field
+      label="profit relative to rent"
+      variant="outlined"
+      suffix="%"
+      v-model="recRent.profit"
+      ></v-text-field>
+      <h3>Yearly fluctuation</h3>
+      <v-container fluid class='pa-0'>
+        <v-row no-gutters>
+          <v-col 
+            cols="4"
+            :key="i"
+            v-for="(item,i) in 12"
+          >
+            <v-text-field
+            background-color="#f0f"
+            variant="outlined"          
+            suffix="%"
+            v-model="recRent.yearlyFluct[i]"
+            :label="(new Date(1993, i, 13)).toLocaleString('default', { month: 'long' })"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+      <h3>Weekly fluctuation</h3>
+      <v-container fluid class='pa-0'>
+        <v-row no-gutters>
+          <v-col 
+          cols="4"
+          :key="i"
+          v-for="(item,i) in 7"
+          >
+            <v-text-field
+            suffix="%"
+            variant="outlined"
+            v-model="recRent.weeklyFluct[i]"
+            :label="(new Date(2022, 8 - 1, 29 - 7 + i)).toLocaleString('default', { weekday: 'long' })"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+      <h3>Daily fluctuation</h3>
+      <v-container fluid class='pa-0'>
+        <v-row no-gutters>
+          <v-col 
+          cols="4"
+          :key="i"
+          v-for="(item,i) in 24"
+          >
+            <v-text-field
+            suffix="%"
+            variant="outlined"
+            v-model="recRent.dailyFluct[i]"
+            :label="i + ':00'"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    
+      <h3>Usage factors</h3>
+      <p>
+        When somebody makes a booking, he will have to state a number for every usage factor. 
+        E.g. number of people. Each additional person will increase the price by f times the price for one person. 
+      </p>
+      <v-container fluid class='pa-0'>
+        <v-row no-gutters
+        v-for="(uf, i) in recRent.usageFactors" 
+        :key="i">
+          <v-col 
+          cols="6">
+            <v-text-field
+            label="name"
+            variant="outlined"
+            v-model="uf.name"></v-text-field>
+          </v-col>
+          <v-col 
+          cols="6">
+            <v-text-field
+            label="factor f"
+            variant="outlined"
+            v-model="uf.f"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>  
+      <v-btn
+          class="button"
+          variant="outlined"
+          @click="recRent.usageFactors.push(new UsageFactor())"
+        >
+        add usage factor
+      </v-btn>
     </template>
+
   </v-form>
 </template>
 
 
 <style lang="less" scoped>
   @import "@/colors";
-  h1 {
-    text-align: center;
-  } 
-  p {
-    margin: 0.5rem 0rem; 
+  h3, p {
+    margin: 1rem ; 
   }
   
-  .calcDetails {
-    p {
-      margin: 0; 
-    }
+  .button {
+    margin: 0 1rem; 
   }
   
   form {
     margin: 2rem 0rem; 
   }
+
   
 </style>
